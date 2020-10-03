afecf7c8559b3870fa318d52c29ab2bfe536fc80
Shashank Baghel <theradcolor@gmail.com>
2020-10-03 04:35:33 +0000

    toolchain: Update to October 3, 2020 build
    GCC VERSION: arm-linux-gnueabi-gcc (GCC) 11.0.0 20201003 (experimental)
    GCC COMMIT URL: https://github.com/gcc-mirror/gcc/commit/b0b9b8f02a1cdf910e63b726db22de99d90b3259
    BUILDER COMMIT: 3d72143a342e25155ae04c0734962d173d113cb9

---
+++ lib/gcc/arm-linux-gnueabi/11.0.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc/configure --enable-languages=c,c++ --target=arm-linux-gnueabi --prefix=/drone/src/builder/arm-linux-gnueabi --disable-multilib --disable-werror CFLAGS='-g0 -O3 -fstack-protector-strong' CXXFLAGS='-g0 -O3 -fstack-protector-strong'";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "arm10tdmi" }, { "tls", "gnu" } };
