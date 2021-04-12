6e5f5b388231233a49f0fe839ff01e82deb94cd6
Shashank Baghel <theradcolor@gmail.com>
2021-04-12 15:07:36 +0530

    Update to https://gcc.gnu.org/git/?p=gcc.git;a=commit;h=c02c39fad02c386f6e687e28282973f580fc95ac, 12042021 build.
    GCC VERSION: arm-linux-gnueabi-gcc (GCC) 11.0.1 20210412 (experimental)
    GCC COMMIT URL: https://gcc.gnu.org/git/?p=gcc.git;a=commit;h=c02c39fad02c386f6e687e28282973f580fc95ac
    BUILDER COMMIT: 1d02ae40180a3a4871b238db6a8d2ed4a72800db

--- lib/gcc/arm-linux-gnueabi/11.0.1/plugin/include/configargs.h
+++ lib/gcc/arm-linux-gnueabi/11.0.1/plugin/include/configargs.h
@@ -1,5 +1,5 @@
 /* Generated automatically. */
-static const char configuration_arguments[] = "../gcc/configure --enable-languages=c,c++ --target=arm-linux-gnueabi --prefix=/drone/src/builder/arm-linux-gnueabi --disable-multilib --disable-werror CFLAGS='-g0 -O3 -fstack-protector-strong' CXXFLAGS='-g0 -O3 -fstack-protector-strong'";
+static const char configuration_arguments[] = "../gcc/configure --enable-languages=c,c++ --target=arm-linux-gnueabi --prefix=/drone/src/builder/arm-linux-gnueabi --disable-nls --disable-multilib --disable-werror CFLAGS='-g0 -O3 -fstack-protector-strong' CXXFLAGS='-g0 -O3 -fstack-protector-strong'";
 static const char thread_model[] = "posix";
 
 static const struct {
