82b79e72ab0cd7ff9a881e59e610604c4141fd7f
Shashank Baghel <theradcolor@gmail.com>
2022-03-17 18:33:23 +0530

    Update to https://gcc.gnu.org/git/?p=gcc.git;a=commit;h=78ef40ecdc1a442587703a4daed9141cf1ab87b2, 17032022 build.
    GCC VERSION: arm-linux-gnueabi-gcc (GCC) 12.0.1 20220317 (experimental)
    GCC COMMIT URL: https://gcc.gnu.org/git/?p=gcc.git;a=commit;h=78ef40ecdc1a442587703a4daed9141cf1ab87b2
    BUILDER COMMIT: 5eb0734385206306b2a74be2a6845ed903f13834

--- arm-linux-gnueabi/include/c++/12.0.1/arm-linux-gnueabi/bits/c++config.h
+++ arm-linux-gnueabi/include/c++/12.0.1/arm-linux-gnueabi/bits/c++config.h
@@ -34,7 +34,7 @@
 #define _GLIBCXX_RELEASE 12
 
 // The datestamp of the C++ library in compressed ISO date format.
-#define __GLIBCXX__ 20220310
+#define __GLIBCXX__ 20220317
 
 // Macros for various attributes.
 //   _GLIBCXX_PURE
