3d627abb57bff98436830755951908cf9df02dc2
Shashank Baghel <theradcolor@gmail.com>
2022-03-24 18:35:38 +0530

    Update to https://gcc.gnu.org/git/?p=gcc.git;a=commit;h=568377743e22c1377d0aaa1ac9113da3ff1b6bd4, 24032022 build.
    GCC VERSION: arm-linux-gnueabi-gcc (GCC) 12.0.1 20220324 (experimental)
    GCC COMMIT URL: https://gcc.gnu.org/git/?p=gcc.git;a=commit;h=568377743e22c1377d0aaa1ac9113da3ff1b6bd4
    BUILDER COMMIT: 5eb0734385206306b2a74be2a6845ed903f13834

--- arm-linux-gnueabi/include/c++/12.0.1/arm-linux-gnueabi/bits/c++config.h
+++ arm-linux-gnueabi/include/c++/12.0.1/arm-linux-gnueabi/bits/c++config.h
@@ -34,7 +34,7 @@
 #define _GLIBCXX_RELEASE 12
 
 // The datestamp of the C++ library in compressed ISO date format.
-#define __GLIBCXX__ 20220317
+#define __GLIBCXX__ 20220324
 
 // Macros for various attributes.
 //   _GLIBCXX_PURE
--- lib/gcc/arm-linux-gnueabi/12.0.1/plugin/include/version.h
+++ lib/gcc/arm-linux-gnueabi/12.0.1/plugin/include/version.h
@@ -9,7 +9,7 @@
 BASEVER, DATESTAMP, DEVPHASE, and REVISION are defined by the
 Makefile.  */
 
-#define version_string "12.0.1 20220317 (experimental)"
+#define version_string "12.0.1 20220324 (experimental)"
 #define pkgversion_string "(GCC) "
 
 /* This is the location of the online document giving instructions for
